a9d7fcaba923cc8747ab6381027b80542701e308
May <maypham@utexas.edu>
2023-07-08 19:33:01 -0500

    added html

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # code-quiz
 Challenge 04 - Web APIs
+
+## Description
+<!-- The portfolio was created to showcase May Pham's skills, projects, and experiences while attending the UT Austin coding bootcamp. The purpose of this personal portfolio website is to present a professional and visually appealing showcase of May Pham's work to potential clients, employers, or collaborators. The site includes projects and information regarding how to get in contact with May Pham. -->
+
+## Usage
+
+
+https://mayphamx.github.io/maypham-code-quiz/
+https://github.com/mayphamx/code-quiz
---
+++ assets/script.js
@@ -0,0 +1,27 @@
+// window.alert("Hi! Welcome to my code quiz :)");
+// var weekDay = window.prompt("What is your name?");
+// console.log(weekDay);
+// var fun = window.confirm("Are you ready?");
+// console.log(fun);
+
+// let start = document.querySelector("#start");
+
+// start.addEventListener('click', function() {
+// 	let timerId = setInterval(function() {
+// 		console.log('!')
+// 	}, 1000);
+// });
+
+// array of quiz questions
+var quizQuestions = [
+  {
+    question: "What color is the sky?",
+    options: ["blue", "green", "yellow","red"],
+    answer: 0,
+  },
+  {
+    question: "What color is the grass?",
+    options: ["blue", "green", "yellow","red"],
+    answer: 1,
+  },
+];
