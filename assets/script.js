a0a4f2c181c8ce8be03179d06d45a82ef609f411
May <maypham@utexas.edu>
2023-07-08 21:22:23 -0500

    added js and css

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,6 @@
-// window.alert("Hi! Welcome to my code quiz :)");
-// var weekDay = window.prompt("What is your name?");
-// console.log(weekDay);
-// var fun = window.confirm("Are you ready?");
-// console.log(fun);
+window.alert("Hi! Welcome to my code quiz :)");
+var fun = window.confirm("Please click start and answer the questions by clicking on the answer.");
+console.log(fun);
 
 // let start = document.querySelector("#start");
 
@@ -13,6 +11,9 @@
 // });
 
 // array of quiz questions
+var timerElement = document.getElementById("timer");
+var leaderboards = [];
+
 var quizQuestions = [
   {
     question: "What color is the sky?",
@@ -25,3 +26,77 @@ var quizQuestions = [
     answer: 1,
   },
 ];
+
+let currentQuestionIndex = 0;
+let score = 0;
+let timeLeft = 60;
+let timerInterval;
+var endTime;
+const initialElement = document.getElementById ("initial");
+const startElement = document.getElementById ("start");
+const submitElement = document.getElementById("submit");
+const optionElement = document.getElementById ("option");
+const questionElement = document.getElementById ("question");
+
+startElement.addEventListener("click", startClicked);
+submitElement.addEventListener("click", saveScore);
+submitElement.style.display = "none";
+initialElement.style.display = "none";
+
+
+function startClicked() {
+  setQuestion();
+  startElement.style.display = "none";
+  timerInterval = setInterval(updateTimer, 1000);
+};
+
+function updateTimer() {
+  timeLeft--;
+  timerElement.textContent = timeLeft;
+  if (timeLeft <= 0) {
+    endQuiz();
+  }
+};
+
+// displays quiz questions 
+function setQuestion() {
+  var currentQuestion = quizQuestions[currentQuestionIndex];
+  questionElement.textContent = currentQuestion.question;
+  optionElement.innerHTML = "";
+  for (let i = 0; i < currentQuestion.options.length; i++) {
+    var choice = document.createElement("li");
+    choice.textContent = currentQuestion.options[i];
+    choice.addEventListener("click", () => {
+      checkAnswer(i);
+    });
+    optionElement.appendChild(choice);
+  };
+};
+
+function checkAnswer(answerIndex) {
+  var currentQuestion = quizQuestions[currentQuestionIndex];
+  if (timeLeft <= 0){
+    endQuiz();
+  };
+  if (answerIndex === currentQuestion.answer) {
+    score++;
+  } else {
+    timeLeft -= 10;
+  }
+  currentQuestionIndex++;
+  if (currentQuestionIndex < quizQuestions.length) {
+    setQuestion();
+  } else {
+      endQuiz();
+  }
+};
+
+function endQuiz() {
+  clearInterval(timerInterval);
+  endTime = timeLeft;
+  timerElement.style.display = "none";
+  optionElement.style.display = "none";
+  initialElement.style.display = "block";
+  submitElement.style.display = "block";
+  questionElement.textContent = "You got " + score + " out of 4 questions correct with " + endTime + " seconds left.";
+};
